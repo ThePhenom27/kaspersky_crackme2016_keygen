56e6160f284166ce3e9ed29010faf9b311277f5e
TheCooler <truecooler@gmail.com>
2018-09-19 22:37:14 +0300

    Добавьте файлы проекта.

---
+++ kaspersky_crackme2016_keygen/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kaspersky_crackme2016_keygen
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
