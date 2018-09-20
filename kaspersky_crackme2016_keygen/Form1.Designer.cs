dabd3d2c34d5865143b9b4ea8e489c66163d621f
TheCooler <truecooler@gmail.com>
2018-09-20 04:15:03 +0300

    первый коммит

--- kaspersky_crackme2016_keygen/Form1.Designer.cs
+++ kaspersky_crackme2016_keygen/Form1.Designer.cs
@@ -28,13 +28,116 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.buttonGeneratePass = new System.Windows.Forms.Button();
+			this.textBoxEmail = new System.Windows.Forms.TextBox();
+			this.textBoxPass = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.buttonValidateCredentials = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonGeneratePass
+			// 
+			this.buttonGeneratePass.Location = new System.Drawing.Point(48, 141);
+			this.buttonGeneratePass.Name = "buttonGeneratePass";
+			this.buttonGeneratePass.Size = new System.Drawing.Size(150, 23);
+			this.buttonGeneratePass.TabIndex = 0;
+			this.buttonGeneratePass.Text = "Generate password";
+			this.buttonGeneratePass.UseVisualStyleBackColor = true;
+			this.buttonGeneratePass.Click += new System.EventHandler(this.buttonGeneratePass_Click);
+			// 
+			// textBoxEmail
+			// 
+			this.textBoxEmail.Location = new System.Drawing.Point(114, 65);
+			this.textBoxEmail.Name = "textBoxEmail";
+			this.textBoxEmail.Size = new System.Drawing.Size(201, 20);
+			this.textBoxEmail.TabIndex = 1;
+			this.textBoxEmail.Text = "test";
+			// 
+			// textBoxPass
+			// 
+			this.textBoxPass.Location = new System.Drawing.Point(114, 91);
+			this.textBoxPass.Name = "textBoxPass";
+			this.textBoxPass.Size = new System.Drawing.Size(201, 20);
+			this.textBoxPass.TabIndex = 2;
+			this.textBoxPass.Text = "11111111111111111111111111111111";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(71, 68);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(34, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "email:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(53, 94);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(55, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "password:";
+			// 
+			// buttonValidateCredentials
+			// 
+			this.buttonValidateCredentials.Location = new System.Drawing.Point(204, 141);
+			this.buttonValidateCredentials.Name = "buttonValidateCredentials";
+			this.buttonValidateCredentials.Size = new System.Drawing.Size(111, 23);
+			this.buttonValidateCredentials.TabIndex = 5;
+			this.buttonValidateCredentials.Text = "Validate credentials";
+			this.buttonValidateCredentials.UseVisualStyleBackColor = true;
+			this.buttonValidateCredentials.Click += new System.EventHandler(this.buttonValidateCredentials_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(48, 196);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(267, 23);
+			this.progressBar1.TabIndex = 6;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 175);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "label3";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(378, 231);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.buttonValidateCredentials);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxPass);
+			this.Controls.Add(this.textBoxEmail);
+			this.Controls.Add(this.buttonGeneratePass);
+			this.Name = "Form1";
+			this.Text = "Kaspersky crackme2016 keygen by TheCooler";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button buttonGeneratePass;
+		private System.Windows.Forms.TextBox textBoxEmail;
+		private System.Windows.Forms.TextBox textBoxPass;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button buttonValidateCredentials;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label3;
 	}
 }
 
--- kaspersky_crackme2016_keygen/Form1.cs
+++ kaspersky_crackme2016_keygen/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+
 
 namespace kaspersky_crackme2016_keygen
 {
@@ -15,6 +17,162 @@ namespace kaspersky_crackme2016_keygen
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+
+		byte[] kaspersky_algo(string pass)
+		{
+			uint block1 = BitConverter.ToUInt32(StringToByteArray(pass.Substring(0, 8)), 0);
+			uint block2 = BitConverter.ToUInt32(StringToByteArray(pass.Substring(8, 8)), 0);
+			uint block3 = BitConverter.ToUInt32(StringToByteArray(pass.Substring(16, 8)), 0);
+			uint block4 = BitConverter.ToUInt32(StringToByteArray(pass.Substring(24, 8)), 0);
+
+
+			uint v5 = block1 & 0xFF0000;     // //algo start for pass
+			uint v6 = (((block1 >> 16) | v5) >> 8);
+			uint v7 = block1 << 16;
+			byte[] block1_byte_array = BitConverter.GetBytes(((block1 & 0xFF00 | v7) << 8) | v6) ;// 1 block assigned
+
+			uint v8 = block2 & 0xFF0000;
+			uint v9 = (((block2) >> 16) | v8) >> 8;
+			uint v10 = block2 << 16;
+			byte[] block2_byte_array = BitConverter.GetBytes((block2 & 0xFF00 | v10 << 8) | v9);// 2 block assigned
+			uint v11 = block3 & 0xFF0000;
+			uint v12 = ((block3 >> 16) | v11) >> 8;
+			uint v13 = block3 << 16;
+			//uint v14 = (int)(v2 + 24);
+			byte[] block3_byte_array = BitConverter.GetBytes(((block3 & 0xFF00 | v13) << 8) | v12);// 3 block assigned
+			uint v15 = block4 & 0xFF0000;
+			uint v16 = ((block4 >> 16) | v15) >> 8;
+			uint v17 = block4 << 16;
+			byte[] block4_byte_array = BitConverter.GetBytes(((block4 & 0xFF00 | v17) << 8) | v16);// 4 block assigned
+
+			return block1_byte_array.Concat(block2_byte_array).Concat(block3_byte_array).Concat(block4_byte_array).ToArray();
+
+
+		}
+
+
+		//byte[] StringToByteArray(string str)
+		//{
+		//	byte[] result = new byte[str.Length];
+		//	foreach (var p in str.Split(' ').Select((element, index) => new { element,index }) )
+		//	{
+		//		result[p.index] = Convert.ToByte(p.element);
+		//	}
+		//	return result;
+		//}
+
+		public static byte[] StringToByteArray(string hex)
+		{
+			return Enumerable.Range(0, hex.Length)
+							 .Where(x => x % 2 == 0)
+							 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+							 .ToArray();
+		}
+
+
+		string[] hardcoded_hashes = {
+				"CD64341F68CEDE586C9693EDC505F378",
+				"05D34B65E96F1D39CCB584C00372D8A3",
+				"A15B7EBAAD915326241DFB5B1BBC546F",
+				"3BA00294E441051554FD64A729114FC9",
+				"8D0AA0050DC9D654200A3D1649DE9D36" };
+
+		MD5 md5 = MD5.Create();
+		bool CrackmeValidate(string email,string password)
+		{
+			
+
+
+			byte[] kas_algo_result = kaspersky_algo(password);
+			byte[] login_md5 = md5.ComputeHash(Encoding.UTF8.GetBytes(email));
+			byte[] xor_encryption_result = new byte[16];
+			for (uint i = 0; i < 16; i++)
+			{
+				xor_encryption_result[i] = (byte)(kas_algo_result[i] ^ login_md5[i]);
+			}
+
+
+			for (int i = 0; i < 5; i++)
+			{
+				byte[] three_byte_hash = md5.ComputeHash(xor_encryption_result, i * 3, 3);
+				byte[] current_hard_coded_hash = StringToByteArray(hardcoded_hashes[i]);
+				if (!three_byte_hash.SequenceEqual(current_hard_coded_hash))
+				{
+					return false;
+				}
+			}
+
+
+			byte chunk1 = 0;
+			byte chunk2 = 0;
+			byte chunk3 = 0;
+			byte chunk4 = 0;
+			byte it = 0;
+			byte it2 = 0;
+
+			for (int i=0;i< login_md5.Length;i+=4)
+			{
+				chunk1 += login_md5[i];
+				chunk2 += login_md5[i + 3];
+				chunk3 = (byte)(login_md5[i + 1] + it);
+				chunk4 = (byte)(login_md5[i + 2] + it2);
+				it2 = chunk4;
+				it = chunk3;
+
+			}
+
+			return chunk1 + chunk2 + chunk3 + chunk4 == kas_algo_result[15];
+
+		}
+
+		private void buttonValidateCredentials_Click(object sender, EventArgs e)
+		{
+			string email = textBoxEmail.Text;
+			string pass = textBoxPass.Text;
+			if (pass.Length < 32 || !System.Text.RegularExpressions.Regex.IsMatch(pass, @"\A\b[0-9a-fA-F]+\b\Z"))
+			{
+				MessageBox.Show("пароль должен представлять из себя 32 символную hex-строку (0..9 , a..f , A..F каждый)");
+				return;
+			}
+			MessageBox.Show(CrackmeValidate(email, pass) ? "Успех! Пароль подходит к мылу" : "Не успех :( Пароль не подходит к мылу");
+
+		}
+
+		void BrutePassword( )
+		{
+			byte[] hardcoded_hash_as_byte_array = StringToByteArray(hardcoded_hashes[0]);
+			int timestamp = 0;
+			for (int i=0;i<0xffffff;i++)
+			{
+				
+				if (timestamp == 0 || Environment.TickCount - timestamp >= 1000)
+				{
+					timestamp = Environment.TickCount;
+					//label3.Text = i.ToString();
+
+					label3.BeginInvoke((MethodInvoker)(() => label3.Text = i.ToString()));
+					progressBar1.BeginInvoke((MethodInvoker)(() => progressBar1.Value = (int)((float)(i / 0xffffff) * 10)));
+				}
+
+				byte[] three_bytes_buffer = BitConverter.GetBytes(i);
+				byte[] three_bytes_buffer_short =three_bytes_buffer.Take(3).ToArray();
+				byte[] current_hash = md5.ComputeHash(three_bytes_buffer_short);
+				
+
+				if (current_hash.SequenceEqual(hardcoded_hash_as_byte_array))
+				{
+					MessageBox.Show("found! value:" + i);
+				}
+				//Application.DoEvents();
+			}
+		}
+
+		private void buttonGeneratePass_Click(object sender, EventArgs e)
+		{
+			Task.Run(() => BrutePassword());
+			//BrutePassword();
 		}
 	}
 }
